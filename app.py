abe368830545cce296ee69d85481cea48c152fa0
mach-12 <mann.compi@gmail.com>
2024-03-14 15:54:44 +0530

    added gunicorn dep

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def get_kyc_info():
         raise E
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=4500, debug=True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=4500)
